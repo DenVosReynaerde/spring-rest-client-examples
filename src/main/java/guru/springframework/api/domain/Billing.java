71bf29e1f337cba01e0514bc3a613c142cfa93ca
DenVosReynaerde <80472846+DenVosReynaerde@users.noreply.github.com>
2021-08-02 08:17:26 +0200

    moved the pojo's

--- src/main/java/guru/springframework/api/domain/Billing.java
+++ src/main/java/guru/springframework/api/domain/Billing.java
@@ -1,12 +1,10 @@
-package guru.springframework.springrestclientexamples.api.domain;
+package guru.springframework.api.domain;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 @Getter
 @Setter
